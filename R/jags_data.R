1e9dcefa85e4a4c6e05ea889ac4cc66a1c326101
Jonathan Trattner <accounts@jdtrat.com>
2021-04-08 10:49:50 -0400

    Initial commit -- jags_data functions and and jags_modeling functions for a Bayesian linear regression

---
+++ R/jags_data.R
@@ -0,0 +1,119 @@
+#' Get the matrix of predictors
+#'
+#' @param .recipe A recipe object
+#'
+#' @return A matrix of predictors for the model
+#' @export
+#'
+jags_data_predictors <- function(.recipe) {
+
+  data <- jags_data_all(.recipe = .recipe)
+  outcome_var <- subset(summary(.recipe), role == "outcome")[["variable"]]
+
+  # set the predictors
+  predictors <- subset(data, select = !names(data) %in% outcome_var)
+  # add a column of ones
+  predictors <- transform(predictors, ones = 1)
+  # move the column of ones to the front
+  predictors <- predictors[,c("ones", setdiff(names(predictors), "ones"))]
+  # convert to a predictors matrix
+  predictors <- as.matrix(predictors)
+
+  return(predictors)
+}
+
+
+#' Get numbers of predictors
+#'
+#' @param .pred_matrix The object returned by \code{\link{jags_data_predictors}}.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+jags_data_num_preds <- function(.pred_matrix) {
+  ncol(.pred_matrix)
+}
+
+
+#' Get numbers of observations
+#'
+#' @param .recipe A recipe object.
+#'
+#' @return The number of observations
+#' @export
+#'
+#' @examples
+jags_data_num_obs <- function(.recipe) {
+  nrow(.recipe$template)
+}
+
+
+#' Pull the modeling data
+#'
+#' @param .recipe A recipe object.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+jags_data_all <- function(.recipe) {
+  data <- .recipe$template
+}
+
+#' Get the response vector
+#'
+#' @param .recipe A recipe object
+#'
+#' @return
+#' @export
+#'
+#' @examples
+jags_data_response <- function(.recipe) {
+
+  # get the column name for the outcome variable
+  outcome_var <- subset(summary(.recipe), role == "outcome")[["variable"]]
+
+  # get the response vector
+  .recipe[["template"]][[outcome_var]]
+
+}
+
+
+
+#' Prep Matrices
+#'
+#' @param .recipe A recipe object
+#' @param family The type of model to perform (e.g. "lm").
+#'
+#' @return A list to be passed into `jags.model`
+#' @export
+#'
+#' @examples
+jags_data_prep <- function(.recipe, family) {
+
+  # Get the prepped data
+  data <- jags_data_all(.recipe = .recipe)
+
+  response <- jags_data_response(.recipe)
+
+  # get the column name for the outcome variable
+  outcome_var <- subset(summary(.recipe), role == "outcome")[["variable"]]
+
+  predictors <- jags_data_predictors(.training_data = data,
+                                     .outcome_var = outcome_var)
+
+  num_preds <- jags_data_num_preds(.pred_matrix = predictors)
+
+  num_obs <- jags_data_num_obs(.recipe = .recipe)
+
+  output <- list("response" = response,
+                 "predictors" = predictors,
+                 "num_preds" = num_preds,
+                 "num_obs" = num_obs)
+
+  class(output) <- c(family, "list")
+
+  return(output)
+
+}
---
+++ R/jags_model.R
@@ -0,0 +1,35 @@
+#' Define JAGS model
+#'
+#' @param jags_data The output of \code{\link{jags_data_prep}}
+#' @param num_chains How many chains for the MCMC.
+#' @param ... Additional arguments for \link[rjags2]{jags.model}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+jags_model_lm <- function(jags_data, num_chains, ...) {
+
+  rjags::jags.model(file = system.file("reg-linear.txt", package = "mickjaggr"),
+                    data = jags_data,
+                    n.chains = num_chains,
+                    ...)
+
+}
+
+#' Run JAGS models
+#'
+#' @param .jags_model The output of \code{\link{jags_model_define}}
+#' @param num_iter Number of iterations per chain.
+#' @param ... Additional arguments for \link[rjags2]{coda.samples}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+jags_model_run <- function(.jags_model, num_iter, ...) {
+  rjags::coda.samples(model = .jags_model,
+                      variable.names = c("beta", "sigma.sq"),
+                      n.iter = num_iter)
+}
+
