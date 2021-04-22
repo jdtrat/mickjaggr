855ecc2bbb663e4199bd44ab3ecb98bbf8be32f9
Jonathan Trattner <accounts@jdtrat.com>
2021-04-22 18:58:31 -0400

    Add {whisker} dependency and functionality (for poisson regression now only) to have posterior predictive checks.

--- R/jags_model.R
+++ R/jags_model.R
@@ -20,16 +20,47 @@ jags_model_lm <- function(jags_data, num_chains, ...) {
 #' Define JAGS model for a Poisson Regression
 #'
 #' @inheritParams jags_model_lm
-#'
+#' @param checks Functions to perform posterior predictive checks with
 #' @return JAGS model object
 #' @export
 #'
 #' @examples
-jags_model_poisson <- function(jags_data, num_chains, ...) {
-  rjags::jags.model(file = system.file("reg-poisson.txt", package = "mickjaggr"),
-                    data = jags_data,
-                    n.chains = num_chains,
-                    ...)
+jags_model_poisson <- function(jags_data, num_chains, checks = NULL, ...) {
+
+  template <- readLines(system.file("reg-poisson.txt", package = "mickjaggr"))
+
+  if (!is.null(checks)) {
+
+    internal_checks <- paste0("pp.", checks, " <- ", checks, "(response.new[])", collapse = " \n")
+
+    add_predictions <-
+    "
+    for (i in 1:num_obs) {
+
+    response.new[i] ~ dpois(theta[i])
+
+    }
+    "
+    data <- list(pp_checks = paste0(add_predictions, internal_checks))
+  } else if (is.null(checks)) {
+    data <- list(pp_checks = NULL)
+  }
+
+  # create temp file
+  tmpfile <- tempfile(fileext = ".txt")
+
+  # write file with pp_checks
+  writeLines(whisker::whisker.render(template, data), con = tmpfile)
+
+  model <- rjags::jags.model(file = tmpfile,
+                             data = jags_data,
+                             n.chains = num_chains,
+                             ...)
+
+  on.exit(unlink(tmpfile))
+
+  return(model)
+
 }
 
 
@@ -44,7 +75,12 @@ jags_model_poisson <- function(jags_data, num_chains, ...) {
 #' @export
 #'
 #' @examples
-jags_model_run <- function(.jags_model, num_iter, variable_names, ...) {
+jags_model_run <- function(.jags_model, num_iter, variable_names, checks = NULL, ...) {
+
+  if (!is.null(checks)) {
+    variable_names <- c(variable_names, paste0("pp.", checks))
+  }
+
   rjags::coda.samples(model = .jags_model,
                       variable.names = variable_names,
                       n.iter = num_iter,
